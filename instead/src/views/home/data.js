f0e5794318c4805d580744d46f57a9f5ecf9de2a
CamiloCabezas <116015687+CamiloCabezas@users.noreply.github.com>
2024-04-08 21:15:31 -0500

    otros problemas

--- instead/src/views/home/data.js
+++ instead/src/views/home/data.js
@@ -6,6 +6,6 @@ export const data  = [
         id:2 , imgURL:"https://res.cloudinary.com/duzipgmd1/image/upload/v1712627726/Imagen1_rtwlgp.png", description:"Esta landing page fue creada para un emprendimiento de ropa deportiva con el fin de dar un toque mas personalizado a la marca", title:"TAMOE"
     },
     {
-        id:3 , imgURL: "https://res.cloudinary.com/duzipgmd1/image/upload/v1712627759/Imagen2_lq8xio.png", description:"La pagina de Caravana Social ONG fue creada para las personas con discapacidad que estaban en busca de mejorar, aprender y tener una oportunidad de trabajo ", title:"Caravana Social ON"
+        id:3 , imgURL: "https://res.cloudinary.com/duzipgmd1/image/upload/v1712627759/Imagen2_lq8xio.png", description:"La pagina de Caravana Social ONG fue creada para las personas con discapacidad que estaban en busca de mejorar, aprender y tener una oportunidad de trabajo ", title:"Caravana Social ONG"
     }
 ]
