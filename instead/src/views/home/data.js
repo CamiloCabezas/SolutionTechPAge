ae4340ceec3cc1f48dd9766defbcf3ecfd2b1444
CamiloCabezas <116015687+CamiloCabezas@users.noreply.github.com>
2024-04-08 21:06:35 -0500

    resolvi problemas de responsive

--- instead/src/views/home/data.js
+++ instead/src/views/home/data.js
@@ -1,11 +1,11 @@
 export const data  = [
     {
-        id:1 , imgURL:"src/views/home/imagenesCarrusel/Imagen3.png"
+        id:1 , imgURL:"https://firebasestorage.googleapis.com/v0/b/granja-c34cd.appspot.com/o/Imagen3.png?alt=media&token=3490f388-1f42-4e83-9c87-4f31e414abd8"
     },
     {
-        id:2 , imgURL:"src/views/home/imagenesCarrusel/Imagen1.png", description:"Esta landing page fue creada para un emprendimiento de ropa deportiva con el fin de dar un toque mas personalizado a la marca", title:"TAMOE"
+        id:2 , imgURL:"https://res.cloudinary.com/duzipgmd1/image/upload/v1712627726/Imagen1_rtwlgp.png", description:"Esta landing page fue creada para un emprendimiento de ropa deportiva con el fin de dar un toque mas personalizado a la marca", title:"TAMOE"
     },
     {
-        id:3 , imgURL: "src/views/home/imagenesCarrusel/Imagen2.png", description:"La pagina de Caravana Social ONG fue creada para las personas con discapacidad que estaban en busca de mejorar, aprender y tener una oportunidad de trabajo ", title:"Caravana Social ONG"
+        id:3 , imgURL: "https://res.cloudinary.com/duzipgmd1/image/upload/v1712627759/Imagen2_lq8xio.png", description:"La pagina de Caravana Social ONG fue creada para las personas con discapacidad que estaban en busca de mejorar, aprender y tener una oportunidad de trabajo ", title:"Caravana Social ON"
     }
 ]
